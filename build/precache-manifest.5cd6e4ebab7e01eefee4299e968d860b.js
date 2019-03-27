f7da10391fd7651808dd4771c797697a267a8551
ediubaedillah <ubaedillahedi@gmail.com>
2019-03-27 16:44:25 +0700

    build

---
+++ build/precache-manifest.5cd6e4ebab7e01eefee4299e968d860b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9746dd7c99b40d4fb7cf",
+    "url": "/static/js/main.f4485b33.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "9746dd7c99b40d4fb7cf",
+    "url": "/static/css/main.160da052.chunk.css"
+  },
+  {
+    "revision": "ca05abcf8bf74fef3baaf173c008bf41",
+    "url": "/index.html"
+  }
+];
